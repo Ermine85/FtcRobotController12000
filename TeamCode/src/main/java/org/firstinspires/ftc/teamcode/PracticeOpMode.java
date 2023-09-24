fbff7f98ca64a366e676a241a521ce4900e8937e
shane437 <erika.tealy@gmail.com>
2023-09-23 17:58:21 -0700

    Sperated Code Created Robot script to hold functions PracticeOpMode runs basic HiBot movement - needs strafing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeOpMode.java
@@ -28,14 +28,15 @@
  */
 
 package org.firstinspires.ftc.teamcode;
+//package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+//import org.firstinspires.ftc.robotcontroller.external.samples.RobotHardware;
 
 
 /**
@@ -58,81 +59,61 @@ public class PracticeOpMode extends LinearOpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     //Motors F = Front B = Back
-    private DcMotor leftDriveF = null; //left_drive_front
-    private DcMotor leftDriveB = null; //left_drive_back
-    private DcMotor rightDriveF = null; //right_drive_front
-    private DcMotor rightDriveB = null; //right_drive_back
-
+    Robot practiceRobot = new Robot(this);
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+        practiceRobot.init();
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDriveF = hardwareMap.get(DcMotor.class, "left_drive_front");
-        leftDriveB = hardwareMap.get(DcMotor.class, "left_drive_back");
-        rightDriveF = hardwareMap.get(DcMotor.class, "right_drive_front");
-        rightDriveB = hardwareMap.get(DcMotor.class, "right_drive_back");
+
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDriveF.setDirection(DcMotor.Direction.REVERSE);
-        leftDriveB.setDirection(DcMotor.Direction.REVERSE);
-        rightDriveF.setDirection(DcMotor.Direction.FORWARD);
-        rightDriveB.setDirection(DcMotor.Direction.FORWARD);
+
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
-       // while (opModeIsActive()) {
+       while (opModeIsActive()) {
 
             // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            practiceRobot.Move(leftPower, rightPower);
 
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
 
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            //double drive = -gamepad1.left_stick_y;
-            //double turn  =  gamepad1.right_stick_x;
-            //leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            //rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
 
-            // Send calculated power to wheels
-            //leftDriveF.setPower(leftPower);
-            //leftDriveB.setPower(leftPower);
-            //rightDriveF.setPower(rightPower);
-            //rightDriveB.setPower(rightPower);
+            //leftDriveF.setPower(0.5);
+            //leftDriveB.setPower(0.5);
+            //rightDriveF.setPower(0.5);
+            //rightDriveB.setPower(-0.5);
 
-            leftDriveF.setPower(0.5);
-            leftDriveB.setPower(0.5);
-            rightDriveF.setPower(-0.5);
-            rightDriveB.setPower(-0.5);
+            //sleep (2000);
 
-            sleep (2000);
-
-        leftDriveF.setPower(0);
-        leftDriveB.setPower(0);
-        rightDriveF.setPower(0);
-        rightDriveB.setPower(0);
+        //leftDriveF.setPower(0);
+        //leftDriveB.setPower(0);
+        //rightDriveF.setPower(0);
+        //rightDriveB.setPower(0);
 
             // Show the elapsed game time and wheel power.
-            //telemetry.addData("Status", "Run Time: " + runtime.toString());
-            //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.update();
-       // }
+       }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+public class Robot {
+    private DcMotor leftDriveF = null; //left_drive_front
+    private DcMotor leftDriveB = null; //left_drive_back
+    private DcMotor rightDriveF = null; //right_drive_front
+    private DcMotor rightDriveB = null; //right_drive_back
+
+    private LinearOpMode CurrentOpMode = null;
+    public Robot(LinearOpMode opMode) {
+        CurrentOpMode = opMode;
+    }
+    public void init()
+    {
+        leftDriveF = CurrentOpMode.hardwareMap.get(DcMotor.class, "left_drive_front");
+        leftDriveB = CurrentOpMode.hardwareMap.get(DcMotor.class, "left_drive_back");
+        rightDriveF = CurrentOpMode.hardwareMap.get(DcMotor.class, "right_drive_front");
+        rightDriveB = CurrentOpMode.hardwareMap.get(DcMotor.class, "right_drive_back");
+
+        leftDriveF.setDirection(DcMotor.Direction.REVERSE);
+        leftDriveB.setDirection(DcMotor.Direction.REVERSE);
+        rightDriveF.setDirection(DcMotor.Direction.FORWARD);
+        rightDriveB.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void Move(double leftPower, double rightPower)
+    {
+        leftDriveF.setPower(leftPower);
+        leftDriveB.setPower(leftPower);
+        rightDriveF.setPower(rightPower);
+        rightDriveB.setPower(-rightPower);
+    }
+}
