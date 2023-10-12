8117d54c2de5bc8090a036e0cb5b02dcf6a77ddb
shane437 <erika.tealy@gmail.com>
2023-10-11 18:59:35 -0700

    12000 Move Code + Main Linear Script

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot12000.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot12000.java
@@ -2,27 +2,36 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
 public class Robot12000 {
     //Wheel Motors (6 total -- 4 omni -- 2 normal) ~Change to 4 mecanum later~
-    private DcMotor FLeftOmni = null; //left_front_omni
-    private DcMotor FRightOmni = null; //right_front_omni
-    private DcMotor BLeftOmni = null; //left_back_omni
-    private DcMotor BRightOmni = null; //right_back_omni
+    private DcMotor LeftOmni = null; //left_omni
+    private DcMotor RightOmni = null; //right_omni
     private DcMotor LeftWheel = null; //left_wheel
     private DcMotor RightWheel = null; //right_wheel
-    public LinearOpMode OpMode = null;
+
+    private LinearOpMode OpMode = null;
     public Robot12000(LinearOpMode opMode) { OpMode = opMode; }
     public void init()
     {
-        FLeftOmni = OpMode.hardwareMap.get(DcMotor.class, "left_front_omni");
-        FRightOmni = OpMode.hardwareMap.get(DcMotor.class, "right_front_omni");
-        BLeftOmni = OpMode.hardwareMap.get(DcMotor.class, "left_back_omni");
-        BRightOmni = OpMode.hardwareMap.get(DcMotor.class, "right_back_omni");
+        LeftOmni = OpMode.hardwareMap.get(DcMotor.class, "left_omni");
+        RightOmni = OpMode.hardwareMap.get(DcMotor.class, "right_omni");
         LeftWheel = OpMode.hardwareMap.get(DcMotor.class, "left_wheel");
         RightWheel = OpMode.hardwareMap.get(DcMotor.class, "right_wheel");
 
+        LeftOmni.setDirection(DcMotor.Direction.REVERSE);
+        LeftWheel.setDirection(DcMotor.Direction.REVERSE);
+        RightOmni.setDirection(DcMotor.Direction.FORWARD);
+        RightWheel.setDirection(DcMotor.Direction.FORWARD);
 
+    }
+    public void Move(double leftPower, double rightPower)
+    {
+        LeftOmni.setPower(leftPower);
+        LeftWheel.setPower(leftPower);
+        RightOmni.setPower(rightPower);
+        RightWheel.setPower(rightPower);
     }
 
 
